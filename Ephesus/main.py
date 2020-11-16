680121a8734a832fcbc358976461e0ab0f17fe95
oxyu8 <liberalist0449@gmail.com>
2020-11-16 15:55:05 +0900

    modify similarity.py
    類似度の逆数をとった

--- Ephesus/main.py
+++ Ephesus/main.py
@@ -22,127 +22,109 @@ top5_categories = get_user_category_preference(
     movie_description_df, all_reviews_df, 5, 884)
 print(top5_categories)
 
-"""
-各映画同士のコサイン類似度を求める関数.
-映画数✖️映画数の行列を返す.
-1行は映画
-ベクトルの中身はその映画と,　他の全ての映画とのコサイン類似度の値.
-"""
-
 movie_similarity = np.load('../data/movie_similarity.npy')
 
-
-"""
-TODO
-    - タプルのリストを作成する（始点,　終点,　重み）
-    - 重みをsimilarityの逆数とする
-    - 重みつきのネットワーク図を作成
-"""
-# weighted_egde_list = []
+weighted_egde_list = []
 # movies_similarities = compute_movie_similarity(all_reviews_df)
-# for index, row in enumerate(movies_similarities):
-#     tuple_list = []
-#     for _index, similarity in enumerate(row):
-#         if _index >= index + 1:
-#             if similarity >= 0.7:
-#                 weighted_egde_tuple = (index+1, _index+1, similarity)
-#                 tuple_list.append(weighted_egde_tuple)
-#     weighted_egde_list.append(tuple_list)
+for index, row in enumerate(movie_similarity):
+    tuple_list = []
+    for _index, similarity in enumerate(row):
+        if _index >= index + 1:
+            if similarity <= 1 / 0.7:
+                weighted_egde_tuple = (index+1, _index+1, similarity)
+                tuple_list.append(weighted_egde_tuple)
+    weighted_egde_list.append(tuple_list)
 
-# # print(weighted_egde_list)
-# G = nx.Graph()
-# for tuple_list in weighted_egde_list:
-#     G.add_weighted_edges_from(tuple_list)
-# print(nx.dijkstra_path(G, 28, 226, weight='weight'))
-# print(G[28][423]['weight'])
-# nx.draw_networkx(G, node_size=200,
-#                  font_size=4, width=0.2, style='dotted')
-# plt.show()
-
-
-get_all_user_review_numbers(all_reviews_df)
-
-# 各ノードから派生するノード数の配列
-"""
-各ノードが他のどのノードとつながっているかを調べる関数
-ノード　: 映画
-枝     : 映画と映画の間に関連性がある場合,　枝が張られる.
-"""
+# print(weighted_egde_list)
+G = nx.Graph()
+for tuple_list in weighted_egde_list:
+    G.add_weighted_edges_from(tuple_list)
+print(nx.dijkstra_path(G, 28, 226, weight='weight'))
+print(G[28][423]['weight'])
+nx.draw_networkx(G, node_size=200, font_size=4, width=0.2, style='dotted')
+plt.show()
 
 
-def get_each_possess_nodes():
-    possessNodes = []  # 各ノードが持つノードを列挙した二次元配列
-    similarity_criterion = 0.65
-    for idx, i in enumerate(movie_similarity):
-        similar_movies = []
-        for index, similarity_value in enumerate(i):
-            if similarity_value >= similarity_criterion:
-                similar_movies.append(index+1)  # movie_idをappendしていく
-        similar_movies.insert(0, idx+1)  # 各配列の先頭に対象の映画のmovie_idを挿入する.
-        possessNodes.append(similar_movies)
-    return possessNodes
+# get_all_user_review_numbers(all_reviews_df)
+
+# # 各ノードから派生するノード数の配列
+# """
+# 各ノードが他のどのノードとつながっているかを調べる関数
+# ノード　: 映画
+# 枝     : 映画と映画の間に関連性がある場合,　枝が張られる.
+# """
 
 
-possess_nodes = get_each_possess_nodes()
-print(possess_nodes)
-
-"""
-各映画が何本の映画とつながっているかを求める関数 [(movie_id, つながっているノードの数)]
-"""
-
-
-def get_each_possess_nodes_number():
-    possessNodesLengths = []
-    for row in possess_nodes:
-        possessNodesLengths.append(len(row))
-    return sorted(enumerate(possessNodesLengths), key=lambda x: x[1], reverse=True)
+# def get_each_possess_nodes():
+#     possessNodes = []  # 各ノードが持つノードを列挙した二次元配列
+#     similarity_criterion = 0.65
+#     for idx, i in enumerate(movie_similarity):
+#         similar_movies = []
+#         for index, similarity_value in enumerate(i):
+#             if similarity_value >= similarity_criterion:
+#                 similar_movies.append(index+1)  # movie_idをappendしていく
+#         similar_movies.insert(0, idx+1)  # 各配列の先頭に対象の映画のmovie_idを挿入する.
+#         possessNodes.append(similar_movies)
+#     return possessNodes
 
 
-"""
-重要度の低い映画を削除する関数.
-つながりのある映画がない映画を重要度の低い映画とする.
-"""
+# possess_nodes = get_each_possess_nodes()
+# print(possess_nodes)
+
+# """
+# 各映画が何本の映画とつながっているかを求める関数 [(movie_id, つながっているノードの数)]
+# """
 
 
-def get_unused_nodes():
-    unused_nodes = []
-    MAX_MOVIE_ID = 1682
-    for i in range(1, MAX_MOVIE_ID):
-        if len(possess_nodes[i-1]) == 1:
-            unused_nodes.append(i)
-    return unused_nodes
+# def get_each_possess_nodes_number():
+#     possessNodesLengths = []
+#     for row in possess_nodes:
+#         possessNodesLengths.append(len(row))
+#     return sorted(enumerate(possessNodesLengths), key=lambda x: x[1], reverse=True)
 
 
-def show_graph():
-    color_map = []
-    G = nx.Graph()
-    categorized_movies = get_categorized_movies_by_user_preference(
-        movie_description_df, top5_categories, all_reviews_df, 884)
-    categorized_movies_by_selected_category = get_categorized_movies_by_selected_category(
-        'documentary', all_reviews_df, movie_description_df, 884)
-    for node in possess_nodes:
-        nx.add_star(G, node)
-    MAX_MOVIE_ID = 1683
-    for node in range(1, MAX_MOVIE_ID):
-        """
-        見たことあるないで分ける+指定したカテゴリを含む映画で色分け
-        """
-        label = categorized_movies[node-1]
-        if label == 'watch_fave' or label == 'watch_not_fave':
-            color_map.append(get_color_by_user_reference(
-                node, categorized_movies))
-        else:
-            label = categorized_movies_by_selected_category[node-1]
-            color_map.append(get_color_by_selected_category(label))
-
-    unused_nodes = get_unused_nodes()
-    for i in unused_nodes:
-        G.remove_node(i)
-    for i in sorted(unused_nodes, reverse=True):  # reverse=True to prevent offset of index
-        color_map.pop(i)
-    nx.draw_networkx(G, node_color=color_map, node_size=200,
-                     font_size=4, width=0.2, style='dotted')
-    plt.show()
+# """
+# 重要度の低い映画を削除する関数.
+# つながりのある映画がない映画を重要度の低い映画とする.
+# """
+# def get_unused_nodes():
+#     unused_nodes = []
+#     MAX_MOVIE_ID = 1682
+#     for i in range(1, MAX_MOVIE_ID):
+#         if len(possess_nodes[i-1]) == 1:
+#             unused_nodes.append(i)
+#     return unused_nodes
 
 
-show_graph()
+# def show_graph():
+#     color_map = []
+#     G = nx.Graph()
+#     categorized_movies = get_categorized_movies_by_user_preference(
+#         movie_description_df, top5_categories, all_reviews_df, 884)
+#     categorized_movies_by_selected_category = get_categorized_movies_by_selected_category(
+#         'documentary', all_reviews_df, movie_description_df, 884)
+#     for node in possess_nodes:
+#         nx.add_star(G, node)
+#     MAX_MOVIE_ID = 1683
+#     for node in range(1, MAX_MOVIE_ID):
+#         """
+#         見たことあるないで分ける+指定したカテゴリを含む映画で色分け
+#         """
+#         label = categorized_movies[node-1]
+#         if label == 'watch_fave' or label == 'watch_not_fave':
+#             color_map.append(get_color_by_user_reference(
+#                 node, categorized_movies))
+#         else:
+#             label = categorized_movies_by_selected_category[node-1]
+#             color_map.append(get_color_by_selected_category(label))
+
+#     unused_nodes = get_unused_nodes()
+#     for i in unused_nodes:
+#         G.remove_node(i)
+#     for i in sorted(unused_nodes, reverse=True):  # reverse=True to prevent offset of index
+#         color_map.pop(i)
+#     nx.draw_networkx(G, node_color=color_map, node_size=200, font_size=4, width=0.2, style='dotted')
+#     plt.show()
+
+
+# show_graph()
--- Ephesus/similarity.py
+++ Ephesus/similarity.py
@@ -5,6 +5,12 @@ import pandas as pd
 from datasets import all_reviews_df
 from sklearn.metrics import pairwise_distances
 
+"""
+各映画同士のコサイン類似度を求める関数.
+映画数✖️映画数の行列を返す.
+1行は映画
+ベクトルの中身はその映画と,　他の全ての映画とのコサイン類似度の値.
+"""
 
 def compute_movie_similarity() -> np.ndarray:
     # アイテム同士の類似度を計算するためにデータをitem_id✖️user_idの行列に変換する
@@ -17,8 +23,8 @@ def compute_movie_similarity() -> np.ndarray:
         user_rating_matrix[row['item_id'] - 1, row['user_id'] - 1] = row['rating']
     # コサイン類似度によるアイテム同士の類似度の配列
     # ダイクストラ法を適応するために,　類似度の逆数をとる
-    # movies_similarities = 1 / (1 - pairwise_distances(user_rating_matrix, metric='cosine'))
-    movies_similarities = 1 - pairwise_distances(user_rating_matrix, metric='cosine')
+    movies_similarities = 1 / (1 - pairwise_distances(user_rating_matrix, metric='cosine'))
+    # movies_similarities = 1 - pairwise_distances(user_rating_matrix, metric='cosine')
     np.fill_diagonal(movies_similarities, 0)  # 対角線上の要素を0に上書きする
     return movies_similarities
 
