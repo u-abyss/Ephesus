a5c9d195a05a0f66d212d329cec596581923186a
oxyu8 <liberalist0449@gmail.com>
2020-12-13 17:30:09 +0900

    txt to dataframe
    artist_similarity.txtとartsit_data.txtをdataframeに格納した

---
+++ Ephesus/txt2df.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+from show_network import show_graph
+
+
+similarity_df = pd.read_table('../artist_similarity.txt')
+artist_data_df = pd.read_table('../atrist_data.txt')
+
+print(artist_data_df)
+
+# similar_artists_list = []
+
+# for row in similarity_df.itertuples(name=None):
+#     similar_artists = list(row)
+#     similar_artists.pop(0)
+#     similar_artists_list.append(similar_artists)
+
+# show_graph(similar_artists_list)
