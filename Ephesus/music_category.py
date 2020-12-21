f9163a3ff177d08089729b46114655a392ba4283
oxyu8 <liberalist0449@gmail.com>
2020-12-21 21:41:49 +0900

    new feat
    ・.envファイルの追加
    ・アーティスト名からカテゴリを取得する関数の作成

---
+++ Ephesus/music_category.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+from sqlconfig import sql_config
+
+artist_categories = np.load('../data/np_artist_name_category.npy', allow_pickle=True)
+
+sql_config.ping(reconnect=True)
+
+# DB操作のカーソル
+cur = sql_config.cursor()
+
+# 対象とするプレイリストのidを取得
+cur.execute("SELECT id FROM playlist WHERE num_followers >= 1000;")
+
+rows = cur.fetchall()
+playlist_ids = [item[0] for item in rows]
+
+
+def fetch_track_ids(playlist_id):
+    cur.execute("SELECT track_id FROM playlist_track_relation WHERE playlist_id = '%s'" % playlist_id)
+    rows = cur.fetchall()
+    track_ids = [item[0] for item in rows]
+    return track_ids
+
+def fetch_album_ids(track_ids):
+    placeholders = ', '.join(['%s']*len(track_ids))
+    query = "SELECT album_id FROM track WHERE id IN ({})".format(placeholders)
+    cur.execute(query, tuple(track_ids))
+    rows = cur.fetchall()
+    album_ids = [item[0] for item in rows]
+    return album_ids
+
+def fetch_artist_names(album_ids):
+    placeholders = ', '.join(['%s']*len(album_ids))
+    query = "SELECT name FROM album WHERE id IN ({})".format(placeholders)
+    cur.execute(query, tuple(album_ids))
+    rows = cur.fetchall()
+    artist_names = [item[0] for item in rows]
+    return artist_names
+
+def get_artist_categories(lowered_artist_names):
+    categories = []
+    for row in artist_categories:
+        if row[0] in lowered_artist_names:
+            categories.append(row[1])
+    return categories
+
+def find_good_playlist(playlist_ids):
+    good_playlists = []
+    for playlist_id in playlist_ids:
+        track_ids = fetch_track_ids(playlist_id)
+        album_ids = fetch_album_ids(track_ids)
+        artist_names = fetch_artist_names(album_ids)
+        lowered_artist_names = list(map(lambda name: name.lower(), artist_names))
+        artist_categories = get_artist_categories(lowered_artist_names)
+        if len(artist_categories) != 0:
+            good_playlists.append(artist_categories)
+    return good_playlists
+
+print(find_good_playlist(playlist_ids))
+
--- Ephesus/sqlconfig.py
+++ Ephesus/sqlconfig.py
@@ -1,79 +1,23 @@
 import mysql.connector as mydb
-import numpy as np
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-# from txt2df import artist_data_df
-# artist_categories = np.load('../data/np_artist_name_category.npy', allow_pickle=True)
-# print(artist_categories)
+load_dotenv(verbose=True)
 
-artist_categories = np.load('../data/np_artist_name_category.npy', allow_pickle=True)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
-conn = mydb.connect(
-    host="localhost",
-    port="3306",
-    user="root",
-    password="Okyu8-0449",
-    database="spotify_playlist",
+DB_HOST=os.environ.get("DB_HOST")
+DB_PORT=os.environ.get("DB_PORT")
+DB_USER=os.environ.get("DB_USER")
+DB_PASSWORD=os.environ.get("DB_PASSWORD")
+DB_DATABASE=os.environ.get("DB_DATABASE")
+
+sql_config = mydb.connect(
+    host=DB_HOST,
+    port=DB_PORT,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    database=DB_DATABASE,
 )
-
-conn.ping(reconnect=True)
-
-# DB操作のカーソル
-cur = conn.cursor()
-
-# 対象とするプレイリストのidを取得
-cur.execute("SELECT id FROM playlist WHERE num_followers >= 1000;")
-
-rows = cur.fetchall()
-playlist_ids = [item[0] for item in rows]
-
-print(playlist_ids)
-
-test_id = 881739
-
-def fetch_track_ids():
-    cur.execute("SELECT track_id FROM playlist_track_relation WHERE playlist_id = '%s'" % test_id)
-    rows = cur.fetchall()
-    track_ids = [item[0] for item in rows]
-    return track_ids
-
-track_ids = fetch_track_ids()
-
-def fetch_album_ids(track_ids):
-    placeholders = ', '.join(['%s']*len(track_ids))
-    query = "SELECT album_id FROM track WHERE id IN ({})".format(placeholders)
-    cur.execute(query, tuple(track_ids))
-    rows = cur.fetchall()
-    album_ids = [item[0] for item in rows]
-    return album_ids
-
-album_ids = fetch_album_ids(track_ids)
-
-
-def fetch_artist_names(album_ids):
-    placeholders = ', '.join(['%s']*len(album_ids))
-    query = "SELECT name FROM album WHERE id IN ({})".format(placeholders)
-    cur.execute(query, tuple(album_ids))
-    rows = cur.fetchall()
-    artist_names = [item[0] for item in rows]
-    return artist_names
-
-artists_names = fetch_artist_names(album_ids)
-print(artists_names)
-
-artist_categories = np.load('../data/np_artist_name_category.npy', allow_pickle=True)
-
-categories = []
-for row in artist_categories:
-    if row[0] in artists_names:
-        categories.append(row[1])
-
-print(categories)
-
-# categories = map(lambda row: row[0] in artists_names, artist_categories)
-# print(categories)
-
-# for i in categories:
-#     print(i)
-
-
-
