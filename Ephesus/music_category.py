99a9574f23e9da9b0df077a2b3cc6fb54955e258
oxyu8 <liberalist0449@gmail.com>
2020-12-21 21:48:02 +0900

    add comments in music_category.py

--- Ephesus/music_category.py
+++ Ephesus/music_category.py
@@ -15,13 +15,18 @@ cur.execute("SELECT id FROM playlist WHERE num_followers >= 1000;")
 rows = cur.fetchall()
 playlist_ids = [item[0] for item in rows]
 
-
+"""
+プレイリスト内の曲のtrack_idを取得する関数
+"""
 def fetch_track_ids(playlist_id):
     cur.execute("SELECT track_id FROM playlist_track_relation WHERE playlist_id = '%s'" % playlist_id)
     rows = cur.fetchall()
     track_ids = [item[0] for item in rows]
     return track_ids
 
+"""
+track_idから，その曲が含まれるアルバムのalbum_idを取得する関数
+"""
 def fetch_album_ids(track_ids):
     placeholders = ', '.join(['%s']*len(track_ids))
     query = "SELECT album_id FROM track WHERE id IN ({})".format(placeholders)
@@ -30,6 +35,9 @@ def fetch_album_ids(track_ids):
     album_ids = [item[0] for item in rows]
     return album_ids
 
+"""
+album_idからそのアルバムのアーティスト名を取得する関数
+"""
 def fetch_artist_names(album_ids):
     placeholders = ', '.join(['%s']*len(album_ids))
     query = "SELECT name FROM album WHERE id IN ({})".format(placeholders)
@@ -38,6 +46,9 @@ def fetch_artist_names(album_ids):
     artist_names = [item[0] for item in rows]
     return artist_names
 
+"""
+アーティスト名の配列から，各アーティストが属するカテゴリの配列を取得する関数
+"""
 def get_artist_categories(lowered_artist_names):
     categories = []
     for row in artist_categories:
@@ -45,12 +56,17 @@ def get_artist_categories(lowered_artist_names):
             categories.append(row[1])
     return categories
 
+"""
+検証実験に使用できそうなプレイリストを返す関数
+今のところ，アーティスト名からカテゴリを取得できるものを返してる．
+"""
 def find_good_playlist(playlist_ids):
     good_playlists = []
     for playlist_id in playlist_ids:
         track_ids = fetch_track_ids(playlist_id)
         album_ids = fetch_album_ids(track_ids)
         artist_names = fetch_artist_names(album_ids)
+        # 大文字小文字を区別せずに，配列に含まれているかどうかをチェックするため，artist_namesの要素を全て小文字に変換する
         lowered_artist_names = list(map(lambda name: name.lower(), artist_names))
         artist_categories = get_artist_categories(lowered_artist_names)
         if len(artist_categories) != 0:
