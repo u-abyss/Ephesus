84a92a08776f16ded9a12c09a73d99167297a433
oxyu8 <liberalist0449@gmail.com>
2021-01-14 15:17:50 +0900

    new feat
    提案アルゴリズムを適応させるための配列を作成する関数の作成

--- Ephesus/route.py
+++ Ephesus/route.py
@@ -24,17 +24,21 @@ def get_route(targets):
             continue
         previous_index = idx
         target_indexes = []
-        index_weight = [idx]
+        # index_weight = [idx]
+        index_weight = []
         for index, weight in enumerate(movie_similarity_list[idx]):
             if index == previous_index:
                 weight = 0
-                index_weight.append([index, weight])
+                # index_weight.append([index, weight])
+                index_weight.append(weight)
             elif index in passed_index:
                 # TODO: 既存の配列に追加する処理の関数を記述
                 weight = 0
-                index_weight.append([index, weight])
+                # index_weight.append([index, weight])
+                index_weight.append(weight)
             elif weight < 1.5:
-                index_weight.append([index, weight])
+                # index_weight.append([index, weight])
+                index_weight.append(weight)
                 if index not in targets:
                     target_indexes.append(index)
         next_target_indexes.append(target_indexes)
@@ -55,32 +59,40 @@ passed_index = [START_INDEX]
 next_target_index = []
 index_weight_list = []
 
-print(movie_similarity_list[0])
-index_weight = [0]
+# index_weight = [0]
+index_weight = []
 for index, weight in enumerate(movie_similarity_list[START_INDEX]):
     if index == START_INDEX:
         weight = 0
-        index_weight.append([index, weight])
+        # index_weight.append([index, weight])
+        index_weight.append(weight)
     elif weight < 1.5:
-        index_weight.append([index, weight])
+        # index_weight.append([index, weight])
+        index_weight.append(weight)
         next_target_index.append(index)
 index_weight_list.append(index_weight)
-print(index_weight_list)
-print("next_target_index:", next_target_index)
-print("")
 #TODO: 残りのノードの数分の0を配列の末尾に追加
-print("index_weight_list:", index_weight_list)
-print("")
-print("=============================================")
+# print("index_weight_list:", index_weight_list)
 
 next_target_indexes = get_route(next_target_index)
-for row in index_weight_list:
-    print(row)
-    print("")
+# for row in index_weight_list:
+#     print(row)
+#     print("")
 
-print(len(index_weight_list))
-print(index_weight_list[2])
-print(len(passed_index))
+def add_zero(weight_list):
+    index_weight_list = []
+    NODE_NUM = len(passed_index)
+    for row in weight_list:
+        row_num = len(row)
+        if row_num == NODE_NUM:
+            index_weight_list.append(row)
+        else:
+            needed_zero_num = NODE_NUM - row_num
+            add_zero_arr = [0] * needed_zero_num
+            row.extend(add_zero_arr)
+            index_weight_list.append(row)
+    return index_weight_list
 
-print("")
-print(passed_index)
+index_weight_list = add_zero(index_weight_list)
+
+
